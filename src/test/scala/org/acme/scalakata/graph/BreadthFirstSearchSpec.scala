f696090e7a7d9152fa77ed42b51de2b2cd06235d
Lemas Tero <adres.mistrza@gmail.com>
2016-03-17 02:56:57 +0100

    BFS tests all but one as ignored

--- src/test/scala/org/acme/scalakata/graph/BreadthFirstSearchSpec.scala
+++ src/test/scala/org/acme/scalakata/graph/BreadthFirstSearchSpec.scala
@@ -2,25 +2,25 @@ package org.acme.scalakata.graph
 
 import org.scalatest.{MustMatchers, FunSpec}
 
-class DepthFirstSearchSpec  extends FunSpec with MustMatchers {
+class BreadthFirstSearchSpec  extends FunSpec with MustMatchers {
 
   describe("factory method") {
 
     it("throws PathFromEmptyGraph when pass empty Graph") {
       intercept[PathFromEmptyGraph] {
-        DepthFirstSearch(NeighboursListGraph(0), 42)
+        BreadthFirstSearch(NeighboursListGraph(0), 42)
       }
     }
 
     ignore("throws PathFromNotExistingNode when try to construct from not existing node") {
       intercept[PathFromNotExistingNode] {
-        DepthFirstSearch(NeighboursListGraph(1), 1)
+        BreadthFirstSearch(NeighboursListGraph(1), 1)
       }
     }
 
     ignore("throws PathFromNotExistingNode when try to construct from negative node") {
       intercept[PathFromNotExistingNode] {
-        DepthFirstSearch(NeighboursListGraph(1), -1)
+        BreadthFirstSearch(NeighboursListGraph(1), -1)
       }
     }
   }
@@ -28,7 +28,7 @@ class DepthFirstSearchSpec  extends FunSpec with MustMatchers {
   describe("existsPathTo") {
 
     ignore("existsPathTo throws NodeNotFound when given not existing vertex") {
-      val path = DepthFirstSearch(NeighboursListGraph(1), 0)
+      val path = BreadthFirstSearch(NeighboursListGraph(1), 0)
       intercept[NodeNotFound] {
         path.existsPathTo(1)
       }
@@ -40,7 +40,7 @@ class DepthFirstSearchSpec  extends FunSpec with MustMatchers {
     }
 
     ignore("returns false when argument is not connected") {
-      val path = DepthFirstSearch(NeighboursListGraph(2), 0)
+      val path = BreadthFirstSearch(NeighboursListGraph(2), 0)
       path.existsPathTo(1) mustBe false
     }
 
@@ -62,26 +62,26 @@ class DepthFirstSearchSpec  extends FunSpec with MustMatchers {
     }
 
     def assertExistPathBetween(graph: NeighboursListGraph, startNode: Int, destination: Int) =
-      DepthFirstSearch(graph, startNode)
+      BreadthFirstSearch(graph, startNode)
         .existsPathTo(destination) mustBe true
   }
 
   describe("getPathTo") {
 
     ignore("throws NodeNotFound when given not existing node") {
-      val path = DepthFirstSearch(NeighboursListGraph(1), 0)
+      val path = BreadthFirstSearch(NeighboursListGraph(1), 0)
       intercept[NodeNotFound] {
         path.getPathTo(1)
       }
     }
 
     ignore("path from root element to itself consist of one step") {
-      val path = DepthFirstSearch(NeighboursListGraph(13), 0)
+      val path = BreadthFirstSearch(NeighboursListGraph(13), 0)
       path.getPathTo(0) mustBe List(0)
     }
 
     ignore("returns empty list when given element is not connected") {
-      val path = DepthFirstSearch(NeighboursListGraph(13), 0)
+      val path = BreadthFirstSearch(NeighboursListGraph(13), 0)
       path.getPathTo(1) mustBe List.empty[Int]
     }
 
@@ -89,7 +89,7 @@ class DepthFirstSearchSpec  extends FunSpec with MustMatchers {
       val graph = NeighboursListGraph(13)
       graph.addEdge(0, 1)
 
-      val path = DepthFirstSearch(graph, 0)
+      val path = BreadthFirstSearch(graph, 0)
       path.getPathTo(1) mustBe List(0, 1)
     }
 
@@ -97,7 +97,7 @@ class DepthFirstSearchSpec  extends FunSpec with MustMatchers {
       val graph = NeighboursListGraph(13)
       graph.addEdge(0,1)
       graph.addEdge(1,2)
-      val path = DepthFirstSearch(graph, 0)
+      val path = BreadthFirstSearch(graph, 0)
       path.getPathTo(2) mustBe List(0, 1, 2)
     }
 
@@ -105,15 +105,34 @@ class DepthFirstSearchSpec  extends FunSpec with MustMatchers {
       val graph = NeighboursListGraph(13)
       graph.addEdge(1, 2)
       graph.addEdge(1, 0)
-      val path = DepthFirstSearch(graph, 0)
+      val path = BreadthFirstSearch(graph, 0)
       path.getPathTo(2) mustBe List(0, 1, 2)
     }
 
     ignore("getPathTo returns empty list when no edge between given object and root") {
       val graph = NeighboursListGraph(13)
       graph.addEdge(1,2)
-      val path = DepthFirstSearch(graph, 0)
+      val path = BreadthFirstSearch(graph, 0)
       path.getPathTo(2) mustBe List[Int]()
+    }
+
+    ignore("getPathTo traverse first all path then leaves") {
+      /*
+       0 +- 1 -- 3 -- 4
+         |            |
+         +- 2 --------+
+       */
+      val graph = NeighboursListGraph(13)
+      graph.addEdge(0,1)
+      graph.addEdge(0,2)
+      graph.addEdge(1,3)
+      graph.addEdge(3,4)
+      graph.addEdge(2,4)
+
+      val path = BreadthFirstSearch(graph, 0)
+
+      path.getPathTo(4) mustBe List(0, 2, 4)
     }
   }
 }
+
