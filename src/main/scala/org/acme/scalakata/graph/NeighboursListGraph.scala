0c6126ee16c0a98ef4c9662906b43a1dd98a99cd
Waldemar Horbowicz <waldemar.horbowicz@gmail.com>
2016-03-18 10:32:20 +0100

    Test 1 passed

---
+++ src/main/scala/org/acme/scalakata/graph/NeighboursListGraph.scala
@@ -0,0 +1,15 @@
+package org.acme.scalakata.graph
+
+object NeighboursListGraph {
+  def apply(i: Int) = new NeighboursListGraph
+}
+
+class NeighboursListGraph extends Graph {
+  override def getNeighbours(node: Int): List[Int] = ???
+
+  override def addEdge(firstNode: Int, secondNode: Int): Graph = ???
+
+  override def nodesNumber: Int = ???
+
+  override def edgesNumber: Int = ???
+}
