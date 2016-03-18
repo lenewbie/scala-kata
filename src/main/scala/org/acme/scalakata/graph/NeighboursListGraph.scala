229afae030224894c123aca142a16eab04d1f643
Waldemar Horbowicz <waldemar.horbowicz@gmail.com>
2016-03-18 12:17:07 +0100

    Test 12 refactored

--- src/main/scala/org/acme/scalakata/graph/NeighboursListGraph.scala
+++ src/main/scala/org/acme/scalakata/graph/NeighboursListGraph.scala
@@ -6,26 +6,24 @@ object NeighboursListGraph {
 
 class NeighboursListGraph(val nodesNumber: Int) extends Graph {
   private var edges = Set.empty[(Int, Int)]
-  override def getNeighbours(node: Int): List[Int] = if(node < 0) throw new NegativeIndex
-  else {
+  override def getNeighbours(node: Int): List[Int] = {
     validate(node)
     null
   }
 
   private def validate(node: Int): Unit = {
     if(node >= nodesNumber)throw new NoSuchNode
+    if(node < 0) throw new NegativeIndex
   }
 
-  override def addEdge(firstNode: Int, secondNode: Int): Graph =
-    if(firstNode < 0 || secondNode < 0) throw new NegativeIndex
-    else {
-      validate(firstNode)
-      validate(secondNode)
-      for(newEdge <- createEdge(firstNode, secondNode)){
-        edges = edges + newEdge
-      }
-      this
+  override def addEdge(firstNode: Int, secondNode: Int): Graph = {
+    validate(firstNode)
+    validate(secondNode)
+    for(newEdge <- createEdge(firstNode, secondNode)){
+      edges = edges + newEdge
     }
+    this
+  }
 
   private def createEdge(firstNode: Int, secondNode: Int) =
     if (firstNode < secondNode) Some(firstNode -> secondNode)
