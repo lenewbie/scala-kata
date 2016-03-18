d415bb7ad10934be03be184b1e41b2170c5a5d97
Waldemar Horbowicz <waldemar.horbowicz@gmail.com>
2016-03-18 11:04:45 +0100

    Test 4 passed

--- src/main/scala/org/acme/scalakata/graph/NeighboursListGraph.scala
+++ src/main/scala/org/acme/scalakata/graph/NeighboursListGraph.scala
@@ -8,7 +8,8 @@ class NeighboursListGraph(val nodesNumber: Int) extends Graph {
   override def getNeighbours(node: Int): List[Int] = ???
 
   override def addEdge(firstNode: Int, secondNode: Int): Graph =
-    if(firstNode >= nodesNumber || secondNode >= nodesNumber) throw new NoSuchNode
+    if(firstNode < 0 || secondNode < 0) throw new NegativeIndex
+    else if(firstNode >= nodesNumber || secondNode >= nodesNumber) throw new NoSuchNode
     else this
 
   override def edgesNumber: Int = ???
